16106fe7f0bc05f2a40203e25a5348af5957b94d
Shahrukh Alam <alam.shahrukh786@gmail.com>
2020-11-08 22:12:14 +0530

    Adds a HARDCODED_API_KEY to see if it can be extracted from App Binary

--- SecureAPIKeys/ViewController.swift
+++ SecureAPIKeys/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    public let HARDCODED_API_KEY = "12345"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
